4b656ade09fb2563b22e10507ee7a3093c1e6511
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2023-11-22 12:47:34 -0300

    Pin every Alpine dependency version
    ================> EXTRAS <================
    > Branch name:	main

--- 7.4/helpers/alpine/dependencies.sh
+++ 7.4/helpers/alpine/dependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #############################################
 # Alpine dependencies
+#
+# References:
+#   - https://pkgs.alpinelinux.org/packages?name=&branch=v3.16&repo=&arch=x86_64&maintainer=
 #############################################
 
 # Update apk before next steps
@@ -9,28 +12,36 @@ apk upgrade
 
 ## Required dependencies
 apk add --no-cache \
-    git \
-    zip libzip-dev unzip \
-    autoconf \
-    gcc \
-    make \
-    g++ \
-    zlib-dev \
-    gmp gmp-dev \
-    bzip2 bzip2-dev \
-    curl curl-dev \
-    libxml2-dev \
-    libffi libffi-dev \
-    libpng libpng-dev \
-    icu-dev \
-    libintl \
-    gettext gettext-dev \
-    ldb-dev \
-    libldap \
-    openldap-dev \
-    freetds-dev \
-    libpq-dev \
-    sqlite-dev \
+    git=2.36.6-r0 \
+    zip=3.0-r9 \
+    libzip-dev=1.8.0-r1 \
+    unzip=6.0-r9 \
+    autoconf=2.71-r0 \
+    gcc=11.2.1_git20220219-r2 \
+    make=4.3-r0 \
+    g++=11.2.1_git20220219-r2 \
+    zlib-dev=1.2.12-r3 \
+    gmp=6.2.1-r2 \
+    gmp-dev=6.2.1-r2 \
+    bzip2=1.0.8-r1 \
+    bzip2-dev=1.0.8-r1 \
+    curl=8.4.0-r0 \
+    curl-dev=8.4.0-r0 \
+    libxml2-dev=2.9.14-r2 \
+    libffi=3.4.2-r1 \
+    libffi-dev=3.4.2-r1 \
+    libpng=1.6.37-r1 \
+    libpng-dev=1.6.37-r1 \
+    icu-dev=71.1-r2 \
+    libintl=0.21-r2 \
+    gettext=0.21-r2 \
+    gettext-dev=0.21-r2 \
+    ldb-dev=2.4.4-r0 \
+    libldap=2.6.3-r3	 \
+    openldap-dev=2.6.3-r3	 \
+    freetds-dev=1.3.10-r0 \
+    libpq-dev=14.10-r0 \
+    sqlite-dev=3.40.1-r0 \
   --repository=https://dl-cdn.alpinelinux.org/alpine/latest-stable/main
 
 # Clean everything at the end
--- 8.2/helpers/alpine/dependencies.sh
+++ 8.2/helpers/alpine/dependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #############################################
 # Alpine dependencies
+#
+# References:
+#   - https://pkgs.alpinelinux.org/packages?name=&branch=v3.18&repo=&arch=x86_64&maintainer=
 #############################################
 
 # Update apk before next steps
@@ -9,29 +12,47 @@ apk upgrade
 
 ## Required dependencies
 apk add --no-cache \
-    git \
-    zip libzip-dev unzip \
-    autoconf \
-    gcc \
-    make \
-    g++ \
-    zlib-dev \
-    gmp gmp-dev \
-    bzip2 bzip2-dev \
-    curl curl-dev \
-    libxml2 libxml2-dev \
-    libffi libffi-dev \
-    libpng libpng-dev \
-    icu-dev \
-    libintl \
-    gettext gettext-dev \
-    ldb-dev \
-    libldap \
-    openldap-dev \
-    freetds-dev \
-    linux-headers \
-    libpq-dev \
-    sqlite-dev \
+    git=2.40.1-r0 \
+    zip=3.0-r12 \
+    libzip=1.9.2-r2 \
+    libzip-dev=1.9.2-r2 \
+    unzip=6.0-r14 \
+    autoconf=2.71-r2 \
+    gcc=12.2.1_git20220924-r10 \
+    make=4.4.1-r1 \
+    g++=12.2.1_git20220924-r10 \
+    zlib=1.2.13-r1 \
+    zlib-dev=1.2.13-r1 \
+    gmp=6.2.1-r3 \
+    gmp-dev=6.2.1-r3 \
+    bzip2=1.0.8-r5 \
+    bzip2-dev=1.0.8-r5 \
+    curl=8.4.0-r0 \
+    curl-dev=8.4.0-r0 \
+    libxml2=2.11.6-r0 \
+    libxml2-dev=2.11.6-r0 \
+    libffi=3.4.4-r2 \
+    libffi-dev=3.4.4-r2 \
+    libpng=1.6.39-r3 \
+    libpng-dev=1.6.39-r3 \
+    icu=73.2-r2 \
+    icu-dev=73.2-r2 \
+    libintl=0.21.1-r7 \
+    gettext=0.21.1-r7 \
+    gettext-dev=0.21.1-r7 \
+    ldb=2.7.2-r1 \
+    ldb-dev=2.7.2-r1 \
+    libldap=2.6.5-r0 \
+    openldap=2.6.5-r0 \
+    openldap-dev=2.6.5-r0 \
+    freetds=1.3.18-r1 \
+    freetds-dev=1.3.18-r1 \
+    linux-headers=6.3-r0 \
+    libpq=15.5-r0 \
+    libpq-dev=15.5-r0 \
+    sqlite=3.41.2-r2 \
+    sqlite-dev=3.41.2-r2 \
+    openssl=3.1.4-r1 \
   --repository=https://dl-cdn.alpinelinux.org/alpine/latest-stable/main
 
 # Link libxml
