6263769d720d6da2f0ad78828cb0a351f19af81b
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2024-03-07 14:19:08 -0300

    Fixes for vulnerabilities found by Snyk
    https://github.com/devdrops/php-toolbox/actions/runs/8179022111/job/22364507781
    
    ================> EXTRAS <================
    > Branch name:	main

--- 7.4/helpers/alpine/dependencies.sh
+++ 7.4/helpers/alpine/dependencies.sh
@@ -22,6 +22,7 @@ apk add --no-cache \
     make \
     g++ \
     zlib-dev \
+    expat \
     gmp \
     gmp-dev \
     bzip2 \
@@ -41,7 +42,10 @@ apk add --no-cache \
     libldap \
     openldap-dev \
     freetds-dev \
+    postgresql14 \
+    postgresql14-dev \
     libpq-dev \
+    sqlite \
     sqlite-dev \
     openssl \
   --repository=https://dl-cdn.alpinelinux.org/alpine/latest-stable/main
