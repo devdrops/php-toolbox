d59cdbf5756f36c6873ac17a87106cc5a2cc5290
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2024-04-05 16:10:55 -0300

    Extra settings for PHP + missing config for Xdebug
    ================> EXTRAS <================
    > Branch name:	main

--- 8.2/helpers/xdebug/install.sh
+++ 8.2/helpers/xdebug/install.sh
@@ -8,4 +8,4 @@
 pecl install -o -f xdebug-3.3.0
 docker-php-ext-enable xdebug
 rm -rf /tmp/pear
-cp /config/xdebug/xdebug.ini /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+cp /config/xdebug/xdebug.ini "${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini"
